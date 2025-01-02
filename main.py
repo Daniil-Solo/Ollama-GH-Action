639b404a30a95df6ee4fe9d3bde13efc4b9b5711
Daniil Solovjev <80649413+Daniil-Solo@users.noreply.github.com>
2025-01-02 22:59:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from github import Github
+import requests
+from github import Github, PullRequest
 
 
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
@@ -12,5 +13,13 @@ pr = repo.get_pull(PULL_NUMBER)
 with open("description.txt", "w", encoding="utf-8") as f:
     f.write(pr.body)
 last_commit = pr.get_commits()[pr.commits - 1]
+comments = []
+
 for file in pr.get_files():
-    pr.create_review(last_commit, "New answer" + pr.body, "REQUEST_CHANGES", [])
+    file_url = file.blob_url
+    resp = requests.get(file_url)
+    file_content = resp.text
+    new_comment = PullRequest.ReviewComment(path=file.filename, body=file_content)
+    comments.append(new_comment)
+
+pr.create_review(last_commit, "New answer" + pr.body, "REQUEST_CHANGES", comments)
