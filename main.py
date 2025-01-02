10dc5be44e51a84e51c80870a45e6edea6fe1a00
Daniil Solovjev <80649413+Daniil-Solo@users.noreply.github.com>
2025-01-02 21:34:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def sum_array(array: list[int]) -> int:
-  array_sum = 0  
-  for num in array:
-    array_sum += num
-  return arryas_sum + num
+import os
+from github import Github
+
+
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+REPO_NAME = os.environ.get("REPO_NAME")
+PULL_NUMBER = int(os.environ.get("PULL_NUMBER"))
+
+g = Github(login_or_token=GITHUB_TOKEN)
+repo = g.get_repo(REPO_NAME)
+pr = repo.get_pull(PULL_NUMBER)
+with open("description.txt", "w", encoding="utf-8") as f:
+    f.write(pr.body)
