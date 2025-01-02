bf74aaf289b14651dd3051020120f72815364185
Daniil Solovjev <80649413+Daniil-Solo@users.noreply.github.com>
2025-01-02 22:18:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ REPO_NAME = os.environ.get("REPO_NAME")
 PULL_NUMBER = int(os.environ.get("PULL_NUMBER"))
 
 g = Github(login_or_token=GITHUB_TOKEN)
-repo = g.get_repo(REPO_NAME)
+repo = g.get_repo(REPO_NAME)  # Login/Repo
 pr = repo.get_pull(PULL_NUMBER)
 with open("description.txt", "w", encoding="utf-8") as f:
     f.write(pr.body)
+pr.create_comment("New answer" + pr.body)
